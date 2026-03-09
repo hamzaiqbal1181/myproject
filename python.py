895e502cf6458905e0f2c7100b5804a5046ccb7e
hamzaiqbal1181 <hamza.iqbal36401@gmail.com>
2026-03-09 13:51:55 +0500

    Addition

--- python.py
+++ python.py
@@ -1,7 +1,11 @@
-print("Hello, World!")
-def greet(name):
-    return f"Hello, {name}!"
+# 
+# add 2 numbers
+def add_numbers(a, b):
+    return a + b
+
 if __name__ == "__main__":
-    name = input("Enter your name: ")
-    print(greet(name))
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    result = add_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is {result}.")
     
